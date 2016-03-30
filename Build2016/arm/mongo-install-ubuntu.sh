22196e6453a0dff08559fef137543e69a1a76629
rafaelgodinho <rafaelgodinho@live.com>
2016-03-30 16:11:06 -0700

    MongoDB sample for //build2016

---
+++ Build2016/arm/mongo-install-ubuntu.sh
@@ -0,0 +1,14 @@
+# Configure mongodb.list file with the correct location
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
+# Disable THP
+sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
+sudo echo never > /sys/kernel/mm/transparent_hugepage/defrag
+sudo grep -q -F 'transparent_hugepage=never' /etc/default/grub || echo 'transparent_hugepage=never' >> /etc/default/grub
+
+# Install updates
+sudo apt-get -y update
+
+#Install Mongo DB
+sudo apt-get install -y mongodb-org
